af639eebe16f30852f87317970db3a8397c19915
sdoubleday <28907224+sdoubleday@users.noreply.github.com>
2019-09-18 23:36:49 -0400

    initial code commit.

---
+++ dir_{{cookiecutter.delimiter}}/{{cookiecutter.test_name}}.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE [{{cookiecutter.test_schema_name}}].[{{cookiecutter.test_name}}]
+AS
+BEGIN
+	--ASSEMBLE
+	/*Do you need to fake tables and insert rows?*/
+
+	--ACT
+	SELECT 
+	/*Relevant Column List Here*/
+	INTO [{{cookiecutter.test_schema_name}}].ACTUAL
+	FROM [SomeObject];
+
+	--ASSERT
+	CREATE TABLE [{{cookiecutter.test_schema_name}}].EXPECTED (
+		/*Relevant Column Definitions Here*/
+	);
+	INSERT INTO [{{cookiecutter.test_schema_name}}].EXPECTED (
+		/*Relevant Column List Here*/
+	) VALUES (
+		/*Relevant Values Here*/
+	);
+
+	EXECUTE [tSQLt].AssertEqualsTable @Expected = '[{{cookiecutter.test_schema_name}}].EXPECTED', @Actual = '[{{cookiecutter.test_schema_name}}].ACTUAL';
+
+END
+
